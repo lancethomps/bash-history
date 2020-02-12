32484156e44816cfa17459b26afeb48b71b4d3ca
Lance Thompson <lthompson05@gmail.com>
2020-02-12 14:24:54 -0800

    add shellcheck, Makefile, and travis integration

--- bash_history.sh
+++ bash_history.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 BASH_HIST_LOGS="${BASH_HIST_LOGS:-$HOME/.logs/bash_history}"
+# shellcheck disable=SC2016
 BASH_HIST_AWK_CMD='{printf "%s\t%s\t%s\n", substr($3,2), substr($1,1,19), $2}'
 BASH_HIST_MAX_WIDTH="${BASH_HIST_MAX_WIDTH:-225}"
 
@@ -17,8 +18,10 @@ elif test -w "$HOME"; then
     _PWD_PREFIX='root@'
   fi
 
+  # shellcheck disable=SC2016
   log_cmd_pre='_prev_cmd="$(fc -ln -0 2>/dev/null)"'
   log_cmd_post='unset _prev_cmd'
+  # shellcheck disable=SC2016
   log_cmd='if [[ ${_prev_cmd} != "	  "* ]]; then echo "$(python -c "from datetime import datetime;import sys;sys.stdout.write(datetime.now().strftime(\"%Y-%m-%d %H:%M:%S.%f\")[:-3]);")'" $log_tz_offset"$'\t'"$_PWD_PREFIX"'$(pwd)${_prev_cmd}" >> '"$BASH_HIST_LOGS"'/bash-history_$(date "+%Y-%m-%d")_'$(hostname)'.log; fi'
   if test "$(id -u)" -ne 0; then
     ! test -d "$BASH_HIST_LOGS" && mkdir -p "$BASH_HIST_LOGS"
@@ -56,10 +59,10 @@ function __ask_user_to_select_cmd() {
 function hist() {
   local use_pager=true
   local bh_pager="${BASH_HIST_PAGER:-$PAGER}"
-  if test "$1" = '--no-pager'; then
+  if test "${1-}" = '--no-pager'; then
     use_pager=false
     shift
-  elif test "$1" = '--pager'; then
+  elif test "${1-}" = '--pager'; then
     use_pager=true
     shift
   elif test -z "${bh_pager}"; then
@@ -68,7 +71,7 @@ function hist() {
     use_pager=false
   fi
 
-  local size="${1}"
+  local size="${1-}"
   if test -z "$size"; then
     if test "$use_pager" = 'true'; then
       size='500'
@@ -81,8 +84,8 @@ function hist() {
   local curr_size=0
   local out add_out all_logs
   all_logs="$(ls -t "$BASH_HIST_LOGS/bash-history"*.log)"
-  while (($size > $curr_size)); do
-    add_out="$(cat "$(echo "$all_logs" | head "-$file_pos" | tail -1)" | tail "-$remaining_size" | tail -r)"
+  while ((size > curr_size)); do
+    add_out="$(< "$(echo "$all_logs" | head "-$file_pos" | tail -1)" tail "-$remaining_size" | tail -r)"
     add_size="$(echo "$add_out" | wc -l | tr -d '\011\012\015')"
     file_pos="$((file_pos + 1))"
     curr_size="$((curr_size + add_size))"
@@ -104,10 +107,10 @@ function hist() {
 function hist_grep() {
   local use_pager=true
   local bh_pager="${BASH_HIST_PAGER:-$PAGER}"
-  if test "$1" = '--no-pager'; then
+  if test "${1-}" = '--no-pager'; then
     use_pager=false
     shift
-  elif test "$1" = '--pager'; then
+  elif test "${1-}" = '--pager'; then
     use_pager=true
     shift
   elif test -z "${bh_pager}"; then
@@ -116,22 +119,29 @@ function hist_grep() {
     use_pager=false
   fi
   local grep_args=("$@")
-  if test "$1" = '--use-case'; then
+  if test "${1-}" = '--use-case'; then
     shift
     grep_args=("$@")
   elif ! [[ "${*: -1}" =~ [A-Z] ]]; then
     grep_args+=("-i")
   fi
 
+  local old_nullglob all_log_files_temp all_log_files
+  old_nullglob="$(shopt -p nullglob)"
+  shopt -s nullglob
+  all_log_files_temp=("$BASH_HIST_LOGS/bash-history"*.log)
+  mapfile -t all_log_files < <(sort -r <<<"${all_log_files_temp[*]}")
+  eval "$old_nullglob"
+
   if test "$use_pager" = 'true'; then
     {
       echo 'Command'$'\t''Time'$'\t''PWD'
-      command grep --color=auto --no-filename -E "${grep_args[@]}" $(ls "$BASH_HIST_LOGS/bash-history"*.log | sort -r | tr $'\n' ' ') | sort -r | awk -F $'\t' "$BASH_HIST_AWK_CMD"
+      command grep --color=auto --no-filename -E "${grep_args[@]}" "${all_log_files[@]}" | sort -r | awk -F $'\t' "$BASH_HIST_AWK_CMD"
     } | eval "${bh_pager}"
   else
     {
       echo 'Time'$'\t''PWD'$'\t''Command'
-      command grep --color=auto --no-filename -E "${grep_args[@]}" $(ls "$BASH_HIST_LOGS/bash-history"*.log | sort -r | tr $'\n' ' ') | sort
+      command grep --color=auto --no-filename -E "${grep_args[@]}" "${all_log_files[@]}" | sort
     }
   fi
 }
@@ -160,9 +170,9 @@ function hist_grep_exec() {
   fi
   echo "${exec_cmd_full}"
   exec_cmd="$(bash_history_extract_command "${exec_cmd_full}")"
-  history -s $exec_cmd
+  history -s "$exec_cmd"
   echo "Running: $exec_cmd"$'\n''---------------------------------'
-  eval $exec_cmd
+  eval "$exec_cmd"
   return $?
 }
 function hist_grep_pwd() {
